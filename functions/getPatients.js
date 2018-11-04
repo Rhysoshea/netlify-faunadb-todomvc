cee09cc20655c68982fa5af95e750518c620375c
rhys <rhys.shea.10@ucl.ac.uk>
2018-11-04 14:50:22 +0900

    remove App.js code insert getPatients

--- functions/getPatients.js
+++ functions/getPatients.js
@@ -4,28 +4,30 @@ const faunadb = require('faunadb'),
 const client = new faunadb.Client({ secret: 'process.env.FAUNADB_SECRET' });
 
 
-client.query(q.CreateDatabase({ name: "my_app" }))
+client.query(
+  q.Get(
+    q.Match(q.Index("all_patient"), "*")))
   .then((ret) => console.log(ret))
 
-  exports.handler = (event, context, callback) => {
-    const data = JSON.parse(event.body)
-    console.log("Function `get-patient` invoked", data)
-    const patientItem = {
-      data: data
-    }
-    /* construct the fauna query */
-    return client.query(q.Create(q.Ref("classes/patient"), all_patient))
-    .then((response) => {
-      console.log("success", response)
-      return callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(response)
-      })
-    }).catch((error) => {
-      console.log("error", error)
-      return callback(null, {
-        statusCode: 400,
-        body: JSON.stringify(error)
-      })
-    })
-  }
+  // exports.handler = (event, context, callback) => {
+  //   const data = JSON.parse(event.body)
+  //   console.log("Function `get-patient` invoked", data)
+  //   const patientItem = {
+  //     data: data
+  //   }
+  //   /* construct the fauna query */
+  //   return client.query(q.Create(q.Ref("classes/patient"), all_patient))
+  //   .then((response) => {
+  //     console.log("success", response)
+  //     return callback(null, {
+  //       statusCode: 200,
+  //       body: JSON.stringify(response)
+  //     })
+  //   }).catch((error) => {
+  //     console.log("error", error)
+  //     return callback(null, {
+  //       statusCode: 400,
+  //       body: JSON.stringify(error)
+  //     })
+  //   })
+  // }
